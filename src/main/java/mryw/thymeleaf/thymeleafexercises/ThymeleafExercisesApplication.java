bb930f8152158c7de806b09cf2a7ee0af3154d5c
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2023-01-22 22:36:08 +0100

    Initial commit

---
+++ src/main/java/mryw/thymeleaf/thymeleafexercises/ThymeleafExercisesApplication.java
@@ -0,0 +1,13 @@
+package mryw.thymeleaf.thymeleafexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mryw/thymeleaf/thymeleafexercises/ThymeleafExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package mryw.thymeleaf.thymeleafexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
